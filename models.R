e0c8f90eb6d99f792b41d123f73320a2c44bc33f
Rohan Singh <rohan.vishen@gmail.com>
2016-09-03 18:24:07 +0530

    Update models.R

--- models.R
+++ models.R
@@ -1,3 +1,4 @@
+
 #Support Vector Machines
 library(e1071)
 train <- read.csv("train.csv")
@@ -11,3 +12,6 @@ train <- read.csv("train_fac.csv")
 test <- read.csv("test_fac.csv")
 model_rf <- randomForest(Lung_Cancer~ SELECTED FEATURES, data = train, importance = TRUE)
 predict <- predict (model_rf, test, type= "prob")
+
+##Data preprocessing and feature selection can be performed by using several add-on packages from CRAN.
+https://cran.r-project.org/web/views/MachineLearning.html
